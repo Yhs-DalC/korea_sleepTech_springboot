f98501c945830bdd5077456acbcaba07cd7c2e3e
Yhs-DalC <scvthego@gmail.com>
2025-04-29 17:47:18 +0900

    2days

---
+++ src/main/java/com/example/korea_sleepTech_springboot/common/ApiMappingPattern.java
@@ -0,0 +1,9 @@
+package com.example.korea_sleepTech_springboot.common;
+
+// URL 설계 패턴(API Mapping Pattern)
+// : RESYful하게 API경로를 규칙적으로 설계하는 것
+// - 각 Controller의 고유 경로를 지정
+
+public class ApiMappingPattern {
+    public static final String BOOK_API = "/api/v1/book";
+}
